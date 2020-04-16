0462d560959de491718d97f5d1e5696cc3e7f14e
Mohamed_Gnichi <gnichimohamed@gmail.com>
2020-04-16 21:19:06 +0100

    Updates

---
+++ precache-manifest.52636771f734ed97297b739ca55f1954.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce2f0247f50f611fcb61b59dbe325bf8",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "3b41288e5bbac514ae62",
+    "url": "/contactmanager/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d088497def183b32b196",
+    "url": "/contactmanager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "3b41288e5bbac514ae62",
+    "url": "/contactmanager/static/js/2.6bb3f5c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contactmanager/static/js/2.6bb3f5c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d088497def183b32b196",
+    "url": "/contactmanager/static/js/main.6c88391a.chunk.js"
+  },
+  {
+    "revision": "b66b50d483ed3f8f9810",
+    "url": "/contactmanager/static/js/runtime-main.edfb515d.js"
+  }
+]);
